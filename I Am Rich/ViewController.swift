d1a14a7656dd5b904b9937f4238498c76881777a
kokifukui <koki.fukui@imjp.co.jp>
2020-09-10 18:33:15 +0900

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by koki.fukui on 2020/09/10.
+//  Copyright © 2020 koki.fukui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
